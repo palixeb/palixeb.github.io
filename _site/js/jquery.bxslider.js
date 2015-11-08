3cf53897b923d82b2b83fb2b25af59fc04ec8cf7
Phillip Bennett <phill.alixe@gmail.com>
2015-11-08 23:58:17 +0900

    A ton of shit

---
+++ _site/js/jquery.bxslider.js
@@ -0,0 +1,15 @@
+'use strict';
+
+$(function() {
+
+  setInterval(function) {
+    $('#slider .slides').animate({'margin-left': '-720px'}, 1000);
+  }, 3000);
+
+  //setInterval
+  //animate margin-left
+  //if it's last slide, go to position 1 (0px)
+
+  //listen for mouseenter and pause
+  //resume on mouseleave
+});
