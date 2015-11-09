5d2043834c6388ae1688fae4b026e7c65dd792f3
Phillip Bennett <phill.alixe@gmail.com>
2015-11-10 00:31:34 +0900

    still cleaning shit up. added an email form and footer

--- js/creative.js
+++ js/creative.js
@@ -21,7 +21,7 @@
         target: '.navbar-fixed-top',
         offset: 51
     })
-
+    
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
         $('.navbar-toggle:visible').click();
