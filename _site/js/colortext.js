3bcdaa13db7f52b7242cad23d784532c221ae28f
Phillip Bennett <phill.alixe@gmail.com>
2016-03-20 21:09:00 +0900

    adding js and page scrolling to diaspora page

---
+++ _site/js/colortext.js
@@ -0,0 +1,280 @@
+  
+     $(function()
+     {
+      var text = $('.test1').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test1">' + atext[i]+'</span>';
+      }
+      $('.test1').html(newText);
+    });
+
+     $(function()
+     {
+      var text = $('.test2').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test2">' + atext[i]+'</span>';
+      }
+      $('.test2').html(newText);
+    });
+
+     $(function()
+     {
+      var text = $('.test3').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test3">' + atext[i]+'</span>';
+      }
+      $('.test3').html(newText);
+    });
+
+   
+     $(function()
+     {
+      var text = $('.test4').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test4">' + atext[i]+'</span>';
+      }
+      $('.test4').html(newText);
+    });
+
+     $(function()
+     {
+      var text = $('.test5').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test5">' + atext[i]+'</span>';
+      }
+      $('.test5').html(newText);
+    });
+
+  
+     $(function()
+     {
+      var text = $('.test6').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test6">' + atext[i]+'</span>';
+      }
+      $('.test6').html(newText);
+    });
+
+    
+     $(function()
+     {
+      var text = $('.test7').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test7">' + atext[i]+'</span>';
+      }
+      $('.test7').html(newText);
+    });
+
+     $(function()
+     {
+      var text = $('.test8').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test8">' + atext[i]+'</span>';
+      }
+      $('.test8').html(newText);
+    });
+
+     $(function()
+     {
+      var text = $('.test9').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test9">' + atext[i]+'</span>';
+      }
+      $('.test9').html(newText);
+    });
+
+     $(function()
+     {
+      var text = $('.test10').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test10">' + atext[i]+'</span>';
+      }
+      $('.test10').html(newText);
+    });
+
+    
+     $(function()
+     {
+      var text = $('.test11').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test11">' + atext[i]+'</span>';
+      }
+      $('.test11').html(newText);
+    });
+
+     $(function()
+     {
+      var text = $('.test12').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test12">' + atext[i]+'</span>';
+      }
+      $('.test12').html(newText);
+    });
+
+
+     $(function()
+     {
+      var text = $('.test13').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test13">' + atext[i]+'</span>';
+      }
+      $('.test13').html(newText);
+    });
+
+  
+     $(function()
+     {
+      var text = $('.test14').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test14">' + atext[i]+'</span>';
+      }
+      $('.test14').html(newText);
+    });
+
+     $(function()
+     {
+      var text = $('.test15').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test15">' + atext[i]+'</span>';
+      }
+      $('.test15').html(newText);
+    });
+
+    
+     $(function()
+     {
+      var text = $('.test16').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test16">' + atext[i]+'</span>';
+      }
+      $('.test16').html(newText);
+    });
+
+    
+     $(function()
+     {
+      var text = $('.test16').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test16">' + atext[i]+'</span>';
+      }
+      $('.test16').html(newText);
+    });
+
+     $(function()
+     {
+      var text = $('.test17').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test17">' + atext[i]+'</span>';
+      }
+      $('.test17').html(newText);
+    });
+
+    
+     $(function()
+     {
+      var text = $('.test18').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test18">' + atext[i]+'</span>';
+      }
+      $('.test18').html(newText);
+    });
+
+ 
+     $(function()
+     {
+      var text = $('.test19').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test19">' + atext[i]+'</span>';
+      }
+      $('.test19').html(newText);
+    });
+
+ 
+     $(function()
+     {
+      var text = $('.test20').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test20">' + atext[i]+'</span>';
+      }
+      $('.test20').html(newText);
+    });
+
+
+     $(function()
+     {
+      var text = $('.test21').text();
+      var atext = text.split("");
+      var newText = '';
+      for(var i=0; i< atext.length; i++)
+      {
+        newText += '<span class = "test21">' + atext[i]+'</span>';
+      }
+      $('.test21').html(newText);
+    });
+
+  
+
+    
