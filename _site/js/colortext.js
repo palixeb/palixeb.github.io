73360cde8a80ade573aca3681a351c7be7a0d514
Phillip Bennett <phill.alixe@gmail.com>
2016-03-22 06:19:15 +0900

    editing story

--- _site/js/colortext.js
+++ _site/js/colortext.js
@@ -1,5 +1,5 @@
   
-     /* colors for paragraphs */
+     /* origial code */
 /*
      $(function()
      {
@@ -13,270 +13,7 @@
       $('.test1').html(newText);
     });
 
-     $(function()
-     {
-      var text = $('.test2').text();
-      var atext = text.split("");
-      var newText = '';
-      for(var i=0; i< atext.length; i++)
-      {
-        newText += '<span class = "test2">' + atext[i]+'</span>';
-      }
-      $('.test2').html(newText);
-    });
-
-     $(function()
-     {
-      var text = $('.test3').text();
-      var atext = text.split("");
-      var newText = '';
-      for(var i=0; i< atext.length; i++)
-      {
-        newText += '<span class = "test3">' + atext[i]+'</span>';
-      }
-      $('.test3').html(newText);
-    });
-
-   
-     $(function()
-     {
-      var text = $('.test4').text();
-      var atext = text.split("");
-      var newText = '';
-      for(var i=0; i< atext.length; i++)
-      {
-        newText += '<span class = "test4">' + atext[i]+'</span>';
-      }
-      $('.test4').html(newText);
-    });
-
-     $(function()
-     {
-      var text = $('.test5').text();
-      var atext = text.split("");
-      var newText = '';
-      for(var i=0; i< atext.length; i++)
-      {
-        newText += '<span class = "test5">' + atext[i]+'</span>';
-      }
-      $('.test5').html(newText);
-    });
-
-  
-     $(function()
-     {
-      var text = $('.test6').text();
-      var atext = text.split("");
-      var newText = '';
-      for(var i=0; i< atext.length; i++)
-      {
-        newText += '<span class = "test6">' + atext[i]+'</span>';
-      }
-      $('.test6').html(newText);
-    });
-
-    
-     $(function()
-     {
-      var text = $('.test7').text();
-      var atext = text.split("");
-      var newText = '';
-      for(var i=0; i< atext.length; i++)
-      {
-        newText += '<span class = "test7">' + atext[i]+'</span>';
-      }
-      $('.test7').html(newText);
-    });
-
-     $(function()
-     {
-      var text = $('.test8').text();
-      var atext = text.split("");
-      var newText = '';
-      for(var i=0; i< atext.length; i++)
-      {
-        newText += '<span class = "test8">' + atext[i]+'</span>';
-      }
-      $('.test8').html(newText);
-    });
-
-     $(function()
-     {
-      var text = $('.test9').text();
-      var atext = text.split("");
-      var newText = '';
-      for(var i=0; i< atext.length; i++)
-      {
-        newText += '<span class = "test9">' + atext[i]+'</span>';
-      }
-      $('.test9').html(newText);
-    });
-
-     $(function()
-     {
-      var text = $('.test10').text();
-      var atext = text.split("");
-      var newText = '';
-      for(var i=0; i< atext.length; i++)
-      {
-        newText += '<span class = "test10">' + atext[i]+'</span>';
-      }
-      $('.test10').html(newText);
-    });
-
-    
-     $(function()
-     {
-      var text = $('.test11').text();
-      var atext = text.split("");
-      var newText = '';
-      for(var i=0; i< atext.length; i++)
-      {
-        newText += '<span class = "test11">' + atext[i]+'</span>';
-      }
-      $('.test11').html(newText);
-    });
-
-     $(function()
-     {
-      var text = $('.test12').text();
-      var atext = text.split("");
-      var newText = '';
-      for(var i=0; i< atext.length; i++)
-      {
-        newText += '<span class = "test12">' + atext[i]+'</span>';
-      }
-      $('.test12').html(newText);
-    });
-
-
-     $(function()
-     {
-      var text = $('.test13').text();
-      var atext = text.split("");
-      var newText = '';
-      for(var i=0; i< atext.length; i++)
-      {
-        newText += '<span class = "test13">' + atext[i]+'</span>';
-      }
-      $('.test13').html(newText);
-    });
-
-  
-     $(function()
-     {
-      var text = $('.test14').text();
-      var atext = text.split("");
-      var newText = '';
-      for(var i=0; i< atext.length; i++)
-      {
-        newText += '<span class = "test14">' + atext[i]+'</span>';
-      }
-      $('.test14').html(newText);
-    });
-
-     $(function()
-     {
-      var text = $('.test15').text();
-      var atext = text.split("");
-      var newText = '';
-      for(var i=0; i< atext.length; i++)
-      {
-        newText += '<span class = "test15">' + atext[i]+'</span>';
-      }
-      $('.test15').html(newText);
-    });
-
-    
-     $(function()
-     {
-      var text = $('.test16').text();
-      var atext = text.split("");
-      var newText = '';
-      for(var i=0; i< atext.length; i++)
-      {
-        newText += '<span class = "test16">' + atext[i]+'</span>';
-      }
-      $('.test16').html(newText);
-    });
-
-    
-     $(function()
-     {
-      var text = $('.test16').text();
-      var atext = text.split("");
-      var newText = '';
-      for(var i=0; i< atext.length; i++)
-      {
-        newText += '<span class = "test16">' + atext[i]+'</span>';
-      }
-      $('.test16').html(newText);
-    });
-
-     $(function()
-     {
-      var text = $('.test17').text();
-      var atext = text.split("");
-      var newText = '';
-      for(var i=0; i< atext.length; i++)
-      {
-        newText += '<span class = "test17">' + atext[i]+'</span>';
-      }
-      $('.test17').html(newText);
-    });
-
-    
-     $(function()
-     {
-      var text = $('.test18').text();
-      var atext = text.split("");
-      var newText = '';
-      for(var i=0; i< atext.length; i++)
-      {
-        newText += '<span class = "test18">' + atext[i]+'</span>';
-      }
-      $('.test18').html(newText);
-    });
-
- 
-     $(function()
-     {
-      var text = $('.test19').text();
-      var atext = text.split("");
-      var newText = '';
-      for(var i=0; i< atext.length; i++)
-      {
-        newText += '<span class = "test19">' + atext[i]+'</span>';
-      }
-      $('.test19').html(newText);
-    });
-
- 
-     $(function()
-     {
-      var text = $('.test20').text();
-      var atext = text.split("");
-      var newText = '';
-      for(var i=0; i< atext.length; i++)
-      {
-        newText += '<span class = "test20">' + atext[i]+'</span>';
-      }
-      $('.test20').html(newText);
-    });
-
-
-     $(function()
-     {
-      var text = $('.test21').text();
-      var atext = text.split("");
-      var newText = '';
-      for(var i=0; i< atext.length; i++)
-      {
-        newText += '<span class = "test21">' + atext[i]+'</span>';
-      }
-      $('.test21').html(newText);
-    });
-
+     
   */
      /* color for headings can be used multiple times */
 
@@ -293,7 +30,6 @@
     $el.html(newText);
   });
 });
-
 
   $(function() {
   var texts = $('.test1');
@@ -336,5 +72,106 @@
     $el.html(newText);
   });
 });
-/* report text colors */
+
+/* can only be used once but allows tags */
+    $(function() {
+  function spammify(node) {
+    var children = node.childNodes;
+    var replaced = node.cloneNode();
+    replaced.innerHTML = "";
     
+    for (var i = 0; i < children.length; i++) {
+      var child = children[i];
+      
+     
+      if (child.nodeType === 1) {
+        replaced.appendChild(spammify(child));
+      }
+    
+      else if (child.nodeType === 3) {
+        var letters = child.textContent;
+        
+        for (var j = 0; j < letters.length; j++) {
+          var span = document.createElement("SPAN");
+          var text = document.createTextNode(letters[j]);
+          span.className = "apparatus";
+          span.appendChild(text);
+          replaced.appendChild(span);
+        }
+      }
+    }
+    
+    return replaced;
+  }
+  
+  var text = document.querySelector('.apparatus');
+  text.innerHTML = spammify(text).innerHTML;
+});
+
+     $(function() {
+  function spammify(node) {
+    var children = node.childNodes;
+    var replaced = node.cloneNode();
+    replaced.innerHTML = "";
+    
+    for (var i = 0; i < children.length; i++) {
+      var child = children[i];
+      
+     
+      if (child.nodeType === 1) {
+        replaced.appendChild(spammify(child));
+      }
+    
+      else if (child.nodeType === 3) {
+        var letters = child.textContent;
+        
+        for (var j = 0; j < letters.length; j++) {
+          var span = document.createElement("SPAN");
+          var text = document.createTextNode(letters[j]);
+          span.className = "arp87";
+          span.appendChild(text);
+          replaced.appendChild(span);
+        }
+      }
+    }
+    
+    return replaced;
+  }
+  
+  var text = document.querySelector('.arp87');
+  text.innerHTML = spammify(text).innerHTML;
+});
+
+$(function() {
+  function spammify(node) {
+    var children = node.childNodes;
+    var replaced = node.cloneNode();
+    replaced.innerHTML = "";
+    
+    for (var i = 0; i < children.length; i++) {
+      var child = children[i];
+      
+     
+      if (child.nodeType === 1) {
+        replaced.appendChild(spammify(child));
+      }
+    
+      else if (child.nodeType === 3) {
+        var letters = child.textContent;
+        
+        for (var j = 0; j < letters.length; j++) {
+          var span = document.createElement("SPAN");
+          var text = document.createTextNode(letters[j]);
+          span.className = "snr24";
+          span.appendChild(text);
+          replaced.appendChild(span);
+        }
+      }
+    }
+    
+    return replaced;
+  }
+  
+  var text = document.querySelector('.snr24');
+  text.innerHTML = spammify(text).innerHTML;
+});
